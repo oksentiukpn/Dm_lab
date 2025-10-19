28afcc3809d4635ad0fc639019dba3a670ad46d0
oksesaneka22 <okseigri22@gmail.com>
2025-10-19 19:35:37 +0300

    little changes

--- functions_1.py
+++ functions_1.py
@@ -8,7 +8,7 @@ def read_file(file: str) -> list | None:
     This function returns opened file in list type
 
     :param file: str, name of the file
-    :return: list, matrix or None if invalid file
+    :return: list | None, matrix or None if invalid file
     '''
     try:
         with open(file, 'r', encoding='utf-8') as file:
@@ -21,14 +21,13 @@ def read_file(file: str) -> list | None:
     except FileNotFoundError:
         return None
 
-def write_file(matrix: list) -> None:
+def write_file(matrix: list) -> bool:
     '''
-    This function returns opened file in list type
+    This function write matrix in file
 
-    :param file: str, name of the file
-    :return: list, matrix or None if invalid file
+    :param matrix: list, matrix that we need to write to file
+    :return: bool, True if everything is ok, and False if invalid file
     '''
-    print(matrix)
     try:
         with open('result.csv', 'w', encoding='utf-8') as file:
             for i, l in enumerate(matrix): # i - index, l - letter
@@ -36,6 +35,6 @@ def write_file(matrix: list) -> None:
                     matrix[i][j] = str(k)
                 file.write("".join(matrix[i]) + '\n')
     except FileNotFoundError:
-        return None
-    return None
+        return False
+    return True
 ##############################################################
